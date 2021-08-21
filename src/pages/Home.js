36cb7b83130f19439f6bcae404b0ff30509a6db6
natsudragneel5 <alroy.fern7@gmail.com>
2021-08-21 15:21:43 +0530

    displayed show results from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
-
+import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, setInput] = useState('');
-
+  const [results, setResults] = useState(null);
   const onInputChange = ev => {
     setInput(ev.target.value);
   };
 
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onKeyDown = ev => {
@@ -22,6 +20,21 @@ const Home = () => {
     }
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <MainPageLayout>
       <input
@@ -33,6 +46,7 @@ const Home = () => {
       <button typer="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
