c952f189c0d1eb1138a7541847336d6dae536333
natsudragneel5 <alroy.fern7@gmail.com>
2021-08-25 17:16:27 +0530

    replaced useState with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,27 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+    case 'FETCH_FAILED': {
+      return { ...prevState, isLoading: false, error: action.error };
+    }
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  show: null,
+  isLoading: true,
+  error: null,
+};
+
 const Show = () => {
   const { id } = useParams();
-  const [show, setShow] = useState(null);
-  const renderShow = () => {
-    show.map(items => (
-      <div key={items.id}>
-        Name:{items.name}
-        Language:{items.language}
-        Premiered on:{items.premiered}
-      </div>
-    ));
-  };
+
+  const [{ show, isLoading, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            dispatch({ type: 'FETCH_SUCCESS', show: results });
+          }
+        }, 2000);
+        if (isMounted) {
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_FAILED', error: err.message });
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
-
-  return <div>{renderShow}</div>;
+  if (isLoading) {
+    return <div>Data is being loaded</div>;
+  }
+  if (error) {
+    return <div>Error occured:{error}</div>;
+  }
+  return <div>this is show page</div>;
 };
 
 export default Show;
