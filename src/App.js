7d4cedbd50a64a5b5e218e91a40d7feb56200bed
natsudragneel5 <alroy.fern7@gmail.com>
2021-09-01 13:13:27 +0530

    styled nav and title

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import Navs from './components/Navs';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import Show from './pages/Show';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/starred">
-        <Starred />
-      </Route>
-      <Route exact path="/show/:id">
-        <Show />
-      </Route>
-      <Route>Page not foud : ERROR 404</Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+        <Route>Page not foud : ERROR 404</Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
